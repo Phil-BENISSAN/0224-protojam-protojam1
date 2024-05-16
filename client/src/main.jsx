0f1b51db4eb68e45d4f68281a7a2eae7b9b639ad
sachaig <sacha.isambourg01@gmail.com>
2024-05-16 11:19:04 +0200

    Path

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Home from "./pages/Home";
+import Stardex from './pages/Stardex.jsx';
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/Stardex",
+        element: <Stardex />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={router} />);
---
+++ client/src/pages/Stardex.jsx
@@ -0,0 +1,7 @@
+export default function Stardex() {
+    return (
+        <>
+        <h1>bg gg</h1>
+        </>
+    );
+}
